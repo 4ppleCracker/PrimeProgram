52e8ffcc4ff0448e57442c6d465ac00a2693c10d
4pplecracker <4pplecracker@gmail.com>
2017-12-02 15:37:40 +0100

    Initial Commit

---
+++ Prime.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Prime {
+    class Prime {
+        public static bool isPrime(ulong i, bool d, bool c) {
+            DateTime start = DateTime.Now;
+            if(i==0 || i % 2 == 0) {
+                return false;
+            }
+            for(ulong k = 0; k<i; k++) {
+                if (d) Console.WriteLine("Now checking if {0} is a prime compared to {1}", i, k);
+                if(k==0||k==1||k==i) {
+                    continue;
+                }
+                if(i % k == 0) {
+                    return false;
+                }
+            }
+            if(c) Console.WriteLine("Checked if prime in {0}ms", DateTime.Now.Subtract(start).TotalMilliseconds);
+            return true;
+        }
+        public static ulong generateLongPrime(bool d) {
+            DateTime start = DateTime.Now;
+            ulong r;
+            byte[] buffer = new byte[8];
+            Random rand = new Random();
+            do {
+                rand.NextBytes(buffer);
+                r = BitConverter.ToUInt64(buffer, 0);
+            } while(!isPrime(r, d, false));
+            Console.WriteLine("generated a number in {0}ms", DateTime.Now.Subtract(start).TotalMilliseconds);
+            return r;
+        }
+        public static uint generatePrime(bool d) {
+            DateTime start = DateTime.Now;
+            uint r;
+            byte[] buffer = new byte[4];
+            Random rand = new Random();
+            do {
+                rand.NextBytes(buffer);
+                r = BitConverter.ToUInt32(buffer, 0);
+            } while(!isPrime(r, d, false));
+            Console.WriteLine("generated a number in {0}ms", DateTime.Now.Subtract(start).TotalMilliseconds);
+            return r;
+        }
+        public static UInt16 generateShortPrime(bool d) {
+            DateTime start = DateTime.Now;
+            UInt16 r;
+            byte[] buffer = new byte[2];
+            Random rand = new Random();
+            do {
+                rand.NextBytes(buffer);
+                r = BitConverter.ToUInt16(buffer, 0);
+            } while(!isPrime(r, d, false));
+            Console.WriteLine("generated a number in {0}ms", DateTime.Now.Subtract(start).TotalMilliseconds);
+            return r;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Prime.Prime;
+
+namespace Playground {
+    public class Program {
+        static void Main(string[] args) {
+            ulong NumberToCheck;
+            bool debug = false;
+            for(;;) {
+                Console.Write(">> ");
+                string Read = Console.ReadLine().ToLower();
+                if (Read == "exit") break;
+                if(Read == "gen 8") {
+                    NumberToCheck = generateLongPrime(debug);
+                    Console.WriteLine("Generated the prime {0}", NumberToCheck);
+                    continue;
+                } else if(Read == "gen 4") {
+                    NumberToCheck = generatePrime(debug);
+                    Console.WriteLine("Generated the prime {0}", NumberToCheck);
+                    continue;
+                } else if(Read == "gen 2") {
+                    NumberToCheck = generateShortPrime(debug);
+                    Console.WriteLine("Generated the prime {0}", NumberToCheck);
+                    continue;
+                } else if(Read == "gen") {
+                    Console.WriteLine("Either type 'gen 2', 'gen 4' or 'gen 8' to get different lengths of prime\n2 is relativly quick(Maximum of half a second) but 4 is really slow(Maximum of 20k seconds) and 8 is incredibly slow(Maximum of 10 Quintillion)\n");
+                    continue;
+                } else if(Read == "debug") {
+                    debug = !debug;
+                    Console.WriteLine("Debug set to {0}", debug);
+                    continue;
+                } else if(Read == "cls") {
+                    Console.Clear();
+                    continue;
+                } else {
+                    if(!UInt64.TryParse(Read, out NumberToCheck)) {
+                        Console.WriteLine("Either type in a number to check if it's a prime or type 'gen' to get information about how to use the prime generator\nType 'debug' to toggle debug mode");
+                        continue;
+                    }
+                }
+                bool Result = isPrime(NumberToCheck, debug, true);
+                Console.WriteLine("{0} is{1}prime", NumberToCheck, Result ? " " : " not ");
+            }
+        }
+    }
+}
